ef3935336b1aac9dd869ade83d0436fbda713b28
omarzaarour <zarour.omar@gmail.com>
2017-03-13 14:09:04 -0600

    add sample,samples view models and mainVM methods

--- SketchRecognition/SketchTest/Viewmodels/MainVM.cs
+++ SketchRecognition/SketchTest/Viewmodels/MainVM.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace SketchTest.Viewmodels
 {
-    class MainVM
+    public class MainVM
     {
+        SamplesVM _samplesVM = null;
+        public SamplesVM SamplesVM
+        {
+            get
+            {
+                return _samplesVM;
+            }
+
+            set
+            {
+                _samplesVM = value;
+            }
+        }
+
+        public MainVM()
+        {
+            SamplesVM = new SamplesVM();
+        }
+
+
+        public void AddSample(Sample sample)
+        {
+            SamplesVM.Samples.Add(sample);
+        }
+
+        public void DeleteSample(Guid sampleID)
+        {
+            Sample s = SamplesVM.Samples.FirstOrDefault(x => x.SampleID == sampleID);
+            if (s != null)
+            {
+                SamplesVM.Samples.Remove(s);
+            }
+        }
+
+        public void DeleteAllSamples()
+        {
+            SamplesVM.Samples.Clear();
+        }
+
+        public void TrainModel()
+        {
+
+        }
+
+        public string Recognize(EntryPointNotFoundException[] pathPoints)
+        {
+            return "";
+        }
+
+        public string[] Recognize(EntryPointNotFoundException[] pathPoints, int topCount)
+        {
+            return null;
+        }
+
     }
 }
--- SketchRecognition/SketchTest/Viewmodels/SamplesVM.cs
+++ SketchRecognition/SketchTest/Viewmodels/SamplesVM.cs
@@ -1,12 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SketchTest.Viewmodels
 {
-    class SamplesVM
+    public class SamplesVM
     {
+        public SamplesVM()
+        {
+            Samples = new ObservableCollection<Sample>();
+        }
+        ObservableCollection<Sample> _samples;
+        public ObservableCollection<Sample> Samples
+        {
+            get
+            {
+                return _samples;
+            }
+
+            set
+            {
+                _samples = value;
+            }
+        }
+    }
+
+    public class Sample
+    {
+        Guid _sampleID = Guid.Empty;
+        Point[] _pathPoints = null;
+        string _label = "";
+        string _tag = "";
+
+
+        public Sample(Guid ID, Point[] pathPoints, string label, string tag)
+        {
+            this.SampleID = ID;
+            this.PathPoints = pathPoints;
+            this.Label = label;
+            this.Tag = tag;
+        }
+
+        public Guid SampleID
+        {
+            get
+            {
+                return _sampleID;
+            }
+
+            set
+            {
+                _sampleID = value;
+            }
+        }
+
+        public Point[] PathPoints
+        {
+            get
+            {
+                return _pathPoints;
+            }
+
+            set
+            {
+                _pathPoints = value;
+            }
+        }
+
+        public string Label
+        {
+            get
+            {
+                return _label;
+            }
+
+            set
+            {
+                _label = value;
+            }
+        }
+
+        public string Tag
+        {
+            get
+            {
+                return _tag;
+            }
+
+            set
+            {
+                _tag = value;
+            }
+        }
+
     }
 }
